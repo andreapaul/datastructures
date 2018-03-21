7dfdb26bb5836d5ebedf1b140d332b5ce5d4d968
andreapaul <34640665+andreapaul@users.noreply.github.com>
2018-03-21 13:28:21 +0530

    Create bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+
+	int i,n,a[100],temp,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+	return 0;
+}
